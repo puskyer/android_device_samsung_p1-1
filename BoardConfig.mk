1d519bd84482f69b8ea20d6684840d14e4a85654
storm31 <sergej.michel@t-online.de>
2014-10-29 21:27:18 +0100

    p1: add lvm support

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -1,5 +1,6 @@
 #
 # Copyright (C) 2009 The Android Open Source Project
+# Copyright (C) 2013 OmniROM Project
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -12,21 +13,16 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-
-#
-# board specific compile-time definitions.
 #
 
 # Import p1-common config
 include device/samsung/p1-common/BoardConfigCommon.mk
 
-BOARD_SYSTEMIMAGE_PARTITION_SIZE := 2010529792
-BOARD_USERDATAIMAGE_PARTITION_SIZE := 477626368
-
+# Board
+BOARD_SYSTEMIMAGE_PARTITION_SIZE := 629145600
+BOARD_USERDATAIMAGE_PARTITION_SIZE := 2013265920
 BOARD_CUSTOM_BOOTIMG_MK := device/samsung/p1/shbootimg.mk
-
 TARGET_RECOVERY_FSTAB := device/samsung/p1/fstab.p1
-
 TARGET_OTA_ASSERT_DEVICE := GT-P1000,GT-P1000L,GT-P1000N,galaxytab,p1,p1l,p1n
 
 # BOARD_USES_HDMI := true
