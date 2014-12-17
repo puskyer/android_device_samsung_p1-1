50ed0e49287d100f1f6c3a3c1f6a702e36b808d3
storm31 <sergej.michel@t-online.de>
2014-12-17 14:07:42 +0100

    - 12.0

--- cm.mk
+++ cm.mk
@@ -6,7 +6,7 @@
 # Inherit CM stuff
 # --------------------------------------------------------------------------------
 
-$(call inherit-product, vendor/cm/config/gsm.mk)
+#$(call inherit-product, vendor/cm/config/gsm.mk)
 $(call inherit-product, vendor/cm/config/common_full_phone.mk)
 
 # Inherit device configuration.
